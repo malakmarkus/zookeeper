ee6145ecd53023dff57ad33e3bdc7dceda1b8306
Malak Markus <malak.markus@outlook.com>
2022-04-29 22:06:42 -0400

    set server and routes

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.listen(PORT, () => {
-    console.log(`API server no on port ${PORT}!`);
-});
+
 const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
@@ -45,7 +43,12 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
-}
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id ===id)[0];
+    return result;
+};
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -53,4 +56,17 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+    res.sendStatus(404);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`API server no on port ${PORT}!`);
 });
