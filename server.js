817c9d7054ae7d7561eb0ebda6782d8328497367
Malak Markus <malak.markus@outlook.com>
2022-04-29 21:00:43 -0400

    fix the port

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
 const app = express();
-app.listen(3001, () => {
-    console.log(`API server no on port 3001!`)
+app.listen(PORT, () => {
+    console.log(`API server no on port ${PORT}!`);
 });
 const { animals } = require('./data/animals.json');
 
