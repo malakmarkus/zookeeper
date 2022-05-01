877db961a7b9ad566b3ead75a29ad7ac5c8ae69c
Malak Markus <malak.markus@outlook.com>
2022-05-01 17:43:29 -0400

    add public folder

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -115,6 +117,19 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 });
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`API server no on port ${PORT}!`);
 });
